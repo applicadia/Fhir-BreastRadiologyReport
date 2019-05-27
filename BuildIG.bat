e3b00fadea9d31702ac9c15540ea64ae69840978
Gjoll\Kurt <KurtA@Gjoll.com>
2019-05-27 13:49:26 -0500

    .

--- BuildIG.bat
+++ BuildIG.bat
@@ -2,8 +2,9 @@ pushd ..\shr-cli
 rmdir /s /q out\fhir
 mkdir out\fhir
 xcopy /E /Q ..\Fhir-BreastRadiologyReport\out\fhir .\out\fhir
-yarn run ig:publish
+call yarn run ig:publish
 rmdir /s /q ..\Fhir-BreastRadiologyReport\out\fhir
 mkdir ..\Fhir-BreastRadiologyReport\out\fhir
-copy out\fhir\guide ..\Fhir-BreastRadiologyReport\out\fhir
+xcopy /E /Q out\fhir ..\Fhir-BreastRadiologyReport\out\fhir
+popd
 prompt
