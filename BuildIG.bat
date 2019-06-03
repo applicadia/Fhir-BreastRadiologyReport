ab4dfc28e85f3f9ddae23cb7fb2e7c724c693842
Gjoll <kurta@gjoll.com>
2019-06-03 12:30:53 -0500

    .

--- BuildIG.bat
+++ BuildIG.bat
@@ -1,10 +1,3 @@
-pushd ..\shr-cli
-rmdir /s /q out\fhir
-mkdir out\fhir
-xcopy /E /Q ..\Fhir-BreastRadiologyReport\out\fhir .\out\fhir
+copy ..\shr-cli\package.json .
 call yarn run ig:publish
-rmdir /s /q ..\Fhir-BreastRadiologyReport\out\fhir
-mkdir ..\Fhir-BreastRadiologyReport\out\fhir
-xcopy /E /Q out\fhir ..\Fhir-BreastRadiologyReport\out\fhir
-popd
 prompt
