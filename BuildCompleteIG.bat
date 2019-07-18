a1f626943c62fcd165b28158d829f9468b847a13
Gjoll <kurta@gjoll.com>
2019-07-18 11:21:37 -0500

    Updates to shc.cli 6.2.3. Added code to automatically build all including patch section titles.

---
+++ BuildCompleteIG.bat
@@ -0,0 +1,8 @@
+node ../shr-cli CIMPL
+
+dotnet "C:\Development\Modzilla2\Solutions\FhirKhit\Projects\Tools\FhirKhit.Tools.SDPatch\bin\Debug\netcoreapp2.2\FhirKhit.Tools.SDPatch.dll" "^MITRE\cimpl\Fhir-BreastRadiologyReport\out\fhir\guide\resources\structuredefinition-breastrad-BreastRadiologyDocument.json" "^MITRE\cimpl\Fhir-BreastRadiologyReport\PatchCommands.txt"
+
+copy ..\shr-cli\package.json .
+call yarn run ig:publish
+
+pause
