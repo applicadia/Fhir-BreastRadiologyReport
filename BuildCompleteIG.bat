fcf2ec9d2d86d0ff895ce4d3542ecc007c488797
Gjoll <kurta@gjoll.com>
2019-07-18 15:36:43 -0500

    Copies landing page from Cancer to here

--- BuildCompleteIG.bat
+++ BuildCompleteIG.bat
@@ -1,4 +1,4 @@
-node ../shr-cli CIMPL
+node ../shr-cli -l error CIMPL
 
 dotnet "C:\Development\Modzilla2\Solutions\FhirKhit\Projects\Tools\FhirKhit.Tools.SDPatch\bin\Debug\netcoreapp2.2\FhirKhit.Tools.SDPatch.dll" "^MITRE\cimpl\Fhir-BreastRadiologyReport\out\fhir\guide\resources\structuredefinition-breastrad-BreastRadiologyDocument.json" "^MITRE\cimpl\Fhir-BreastRadiologyReport\PatchCommands.txt"
 
