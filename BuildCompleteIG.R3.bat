7bfb36ef61033f0603bab8a16ef4ea3ce9dd1f21
Gjoll <kurta@gjoll.com>
2019-07-26 11:32:56 -0500

    New profile overviwe image, new sections (Patient History and Prior Studues)

--- BuildCompleteIG.R3.bat
+++ BuildCompleteIG.R3.bat
@@ -1,3 +1,4 @@
+copy "Graphics\ProfileOverview.jpg" "CIMPL\Pages"
 call .\BuildProfiles.v3.bat
 call .\BuildIG.v3.bat
 
--- BuildCompleteIG.R4.bat
+++ BuildCompleteIG.R4.bat
@@ -1,3 +1,4 @@
+copy "Graphics\ProfileOverview.jpg" "CIMPL\Pages"
 call .\BuildProfiles.v4.bat
 call .\BuildIG.v4.bat
 
