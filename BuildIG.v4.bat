b41fa8a9aa3e5242a6307b7b205fb3c35fd343d9
Gjoll <kurta@gjoll.com>
2019-07-26 20:18:36 -0500

    New build with fixed section titles and graphic

--- BuildIG.v4.bat
+++ BuildIG.v4.bat
@@ -1,4 +1,5 @@
 copy ..\shr-cli\package.json .
+copy "Graphics\ProfileOverview.jpg" "out.v4\fhir\guide\pages"
 
 dotnet "C:\Development\Modzilla2\Solutions\FhirKhit\Projects\SDPatch\FhirKhit.SDPatch.R4\bin\Debug\netcoreapp2.2\FhirKhit.Tools.SDPatch.r4.dll" "^MITRE\cimpl\Fhir-BreastRadiologyReport\out.v4\fhir\guide\resources\structuredefinition-breastrad-BreastRadiologyDocument.json" "^MITRE\cimpl\Fhir-BreastRadiologyReport\PatchCommands.txt"
 
